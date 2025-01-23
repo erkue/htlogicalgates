a01d3f57e8a86c63aaa90125ac932546abad42b8
erkue <133379617+erkue@users.noreply.github.com>
2025-01-23 17:29:32 +0100

    [update] add tests and ci (#33)
    resolves #7 #14

---
+++ tests/test_circuit.py
@@ -0,0 +1,12 @@
+import unittest
+
+from htlogicalgates.circuit import *
+
+
+class TestCircuit(unittest.TestCase):
+    def test_constructor(self):
+        c = Circuit(2)
+        self.assertEqual(c.num_qubits, 2)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_tailor_logical_gate.py
@@ -0,0 +1,13 @@
+import unittest
+
+from htlogicalgates import *
+
+
+class TestTailorLogicalGate(unittest.TestCase):
+    def test_tailor_logical_gate(self):
+        conn = Connectivity("circular", n=4)
+        qecc = StabilizerCode("4_2_2")
+        log_gate = Circuit.from_string("H 0", 2)
+        circ, status = tailor_logical_gate(qecc, conn, log_gate, 2)
+        self.assertEqual(status, "Optimal")
+        self.assertEqual(circ.two_qubit_gate_count(), 4)
