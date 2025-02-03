5301854f134d65d76797cde1af263af323f47447
erkue <133379617+erkue@users.noreply.github.com>
2025-02-03 20:16:36 +0100

    [fix] adds comments to circuits and resolves #50

--- tests/test_circuit.py
+++ tests/test_circuit.py
@@ -10,6 +10,18 @@ class TestCircuit(unittest.TestCase):
         c = Circuit("H 2")
         self.assertEqual(c.num_qubits, 3)
 
+    def test_empty_lines_string(self):
+        c = Circuit("H 0\n\nH 1")
+        self.assertEqual(c.gate_count(), 2)
+
+    def test_comment_string(self):
+        c = Circuit("H 0\nCZ 0 1 #Comment\nH 1")
+        self.assertEqual(c.gate_count(), 3)
+
+    def test_no_whitespace_string(self):
+        c = Circuit("H 0\nH1\nCZ 0 1")
+        self.assertEqual(c.gate_count(), 3)
+
     def test_shallow_optimize(self):
         c = Circuit("H 2\nS 2\nCZ 0 2")
         self.assertEqual(c.gate_count(), 3)
